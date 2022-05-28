375027fa30ed37c645add2b9719ad45b76c6ec01
gerald-x <64314917+gerald-x@users.noreply.github.com>
2022-05-28 05:23:02 +0100

    config file

---
+++ config.py
@@ -0,0 +1,5 @@
+SECRET_KEY = "some super long hex"
+MONGO_URI = "mongodb+srv://root:root@cluster0.m6xpd.mongodb.net/flask-auth?retryWrites=true&w=majority"
+MONGODB_SETTINGS = {
+    "host": MONGO_URI
+}
